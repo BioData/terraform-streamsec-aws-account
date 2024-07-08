08e032cf375fb5040cc5eb47201e2dd119830487
Yossi Hai <yossi.hai@biodata.com>
2024-07-08 12:13:54 +0300

    module(iam-activity): add custom tags and remove hardcoded lambda runtime version)

--- modules/iam-activity/variables.tf
+++ modules/iam-activity/variables.tf
@@ -134,6 +134,30 @@ variable "lambda_policy_path" {
   default     = null
 }
 
+#####################################
+# Added by Biodata
+#####################################
+
+variable "lambda_runtime" {
+  description = "(optional) overwrite hardcoded lambda compatible_runtimes and lambda_runtime"
+  type        = string
+  default     = "nodejs20.x"
+  nullable    = false
+}
+
+variable "lambda_tags" {
+  description = "A map of tags to add to the lambda created"
+  type        = map(string)
+  default     = {}
+}
+
+variable "iam_policy_tags" {
+  description = "A map of additional tags to add to the IAM policy created"
+  type        = map(string)
+  default     = {}
+  nullable    = false
+}
+
 ################################################################################
 # IAM Activity S3
 ################################################################################
